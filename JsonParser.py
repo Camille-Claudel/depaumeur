7cdc90eb9ffb4aee4ceb2f34052b7ee4a59c58bd
Someone-42 <tristn.lemarchand@gmail.com>
2022-01-21 19:41:59 +0100

    Cleaner _get_addr function and unpacking

--- JsonParser.py
+++ JsonParser.py
@@ -2,7 +2,8 @@ import json
 import numpy as np
 import Utils
 
-__ADDRESS_START_STRING = "34:8a:12:c" # You can put it in some config file if you want
+# Prefix necessary to keep the address when parsing
+__WANTED_PREFIX = "34:8a:12:c" # You can put it in some config file if you want
 
 def parse_raw_calibrations(json_string: str, mean_function = np.average, keep_last_digit: bool = True):
     """ Returns a Tuple with 1st element : a list of all existing addresses and 2nd element : a dictionnary coords (key) gives a vector of signals (value) """
@@ -10,30 +11,30 @@ def parse_raw_calibrations(json_string: str, mean_function = np.average, keep_la
     addresses = []
     coords_dict = {}
     
-    len_ass = len(__ADDRESS_START_STRING)
+    len_ass = len(__WANTED_PREFIX)
     def _get_addr(full_address: str): # Embedded function i know....
         """ Returns a tuple (is it an address that we care about, the address cut of its prefix) """
         addr_split = (
             address[:len_ass], 
             address[len_ass:] if keep_last_digit else addresses[len_ass:-1]
             ) # Cutting the address in 2 -> (prefix, address)
-        return (addr_split[0] == __ADDRESS_START_STRING, addr_split[1])
+        return (addr_split[0] == __WANTED_PREFIX, addr_split[1])
 
     # First pass over elements to get the final array and coordinates
     for element in data: # Unpacks data according to the raw_calibration_template.json
         for address in element["data"]: # Going over all addresses (same as .keys())
-            addr = _get_addr(address)
-            if (addr[1] not in addresses) and addr[0]: # If the address is not known, and starts with the right prefix
-                addresses.append(addr[1]) # Adding the address, as it doesnt exist
+            starts_with_prefix, addr = _get_addr(address)
+            if (addr not in addresses) and starts_with_prefix: # If the address is not known, and starts with the right prefix
+                addresses.append(addr) # Adding the address, as it doesnt exist
     
     for element in data:
         vector = Utils.create_empty_vector(addresses)
         for address in element["data"]:
-            addr = _get_addr(address)
-            if not addr[0]: continue
+            starts_with_prefix, addr = _get_addr(address)
+            if not starts_with_prefix: continue
             Utils.insert_coord_in_vector(
                 vector, 
-                addr[1], 
+                addr, 
                 mean_function(element["data"][address]), # Getting average of the addresses signal points
                 addresses)
         coords_dict[tuple(element["coords"])] = vector
