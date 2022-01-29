ef24f7c7eebf0b7214a8cb6c2c8e6f8250ee3cf6
RemiMazhar <80780126+RemiMazhar@users.noreply.github.com>
2022-01-29 15:00:54 +0100

    change names
    it actually doesn't get the closest repearter, it gets the closest calibration point

--- get_closest_point.py
+++ get_closest_point.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-def get_closest_repeater(signal,calibration):
-    #give coords of the closest repeater
-    closest_repeater = ( 0, 0)
+def get_closest_point(signal,calibration):
+    #give coords of the closest calibration point
+    closest_point = ( 0, 0)
     closest_distance = 0
     signal_vector = np.array(signal) 
     for i in calibration:
@@ -10,5 +10,5 @@ def get_closest_repeater(signal,calibration):
         dst = np.linalg.norm(calibration_vector - signal_vector) 
         if dst < closest_distance:
             closest_distance = dst
-            closest_repeater = i["coords"]
-    return closest_repeater
+            closest_point = i["coords"]
+    return closest_point
