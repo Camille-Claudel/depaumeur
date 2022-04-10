ae00be164012d21ee9e4da3d456705baa117d0da
Someone-42 <tristn.lemarchand@gmail.com>
2022-04-10 10:31:40 +0200

    IWifiScanner object for platform specific implementations

---
+++ src/OFDepaumer.Game/WifiEvents/IWifiScanner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OFDepaumer.Game.WifiPositioning;
+
+namespace OFDepaumer.Game.WifiEvents
+{
+    public interface IWifiScanner
+    {
+        public bool TryPerformWifiScan(out IWifiSignal[] scannedSignals);
+        bool TryPerformWifiScan(object p, object signals);
+    }
+}
