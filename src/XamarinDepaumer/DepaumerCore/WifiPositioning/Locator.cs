44783aad286569d9994024925bb51e9532c8b620
Someone-42 <tristn.lemarchand@gmail.com>
2022-04-19 10:43:24 +0200

    Locator new interface implementation

--- src/XamarinDepaumer/DepaumerCore/WifiPositioning/Locator.cs
+++ src/XamarinDepaumer/DepaumerCore/WifiPositioning/Locator.cs
@@ -7,11 +7,19 @@ namespace Depaumer.WifiPositioning;
 
 public class Locator : ILocator
 {
+
     public ICalibrationSettings Settings { get; }
+
+    public Vector2? CurrentPosition { get; private set; }
+
+    public TimeOnly LastUpdateTime { get; private set; }
+
+    public event EventHandler<IPositionUpdateArgs>? PositionUpdated;
 
     public Locator(ICalibrationSettings settings)
     {
         Settings = settings;
+        LastUpdateTime = new TimeOnly(DateTime.Now.Ticks);
     }
 
     private double[] makeVector(IWifiSignal[] signals)
@@ -30,13 +38,15 @@ public class Locator : ILocator
 
     public Vector2 Locate(IWifiSignal[] signals)
     {
+
+        // Calculating new position
         double[] measureVector = makeVector(signals);
 
         double minDst = double.MaxValue;
         int argmin = 0;
         for (int i = 0; i < Settings.CalibrationPoints.Length; i++)
         {
-            double dst = Math.ArrayExtensions.SquaredEuclidianDistance(measureVector, Settings.CalibrationPoints[i].Signals);
+            double dst = Utils.ArrayExtensions.SquaredEuclidianDistance(measureVector, Settings.CalibrationPoints[i].Signals);
             if (dst < minDst)
             {
                 minDst = dst;
@@ -44,6 +54,19 @@ public class Locator : ILocator
             }
         }
 
-        return Settings.CalibrationPoints[argmin].Position;
+        // Firing Event and return of new position
+        Vector2 pos = Settings.CalibrationPoints[argmin].Position; // Buffering position
+
+        IPositionUpdateArgs args = new PositionUpdateArgs(
+            CurrentPosition,    // Previous position
+            pos,                // The new calculated distance
+            LastUpdateTime);
+        
+        CurrentPosition = pos;                              // Updating instance variables
+        LastUpdateTime = new TimeOnly(DateTime.Now.Ticks);
+
+        PositionUpdated?.Invoke(this, args);
+
+        return pos;
     }
 }
