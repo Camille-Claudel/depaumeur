8dbf7a6a30ffc3eb46404bfd99d005665142e0c7
Someone-42 <tristn.lemarchand@gmail.com>
2022-04-19 10:45:15 +0200

    XamarinDepaumer Console testing project

---
+++ src/XamarinDepaumer/DepaumerConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Depaumer.Tests;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WifiMain main = new WifiMain();
+
+        Console.WriteLine(main.locator.LastUpdateTime);
+    }
+}
