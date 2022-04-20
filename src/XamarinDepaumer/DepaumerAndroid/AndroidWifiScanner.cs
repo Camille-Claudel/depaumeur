7893f996aec0aae33e45be6cbd4450a6c6fc826b
Someone-42 <tristn.lemarchand@gmail.com>
2022-04-20 11:30:40 +0200

    Wifi scanning implementation attempt

---
+++ src/XamarinDepaumer/DepaumerAndroid/AndroidWifiScanner.cs
@@ -0,0 +1,47 @@
+﻿using Android.Net.Wifi;
+using Depaumer.WifiEvents;
+using Depaumer.WifiPositioning;
+using System;
+using System.Collections.Generic;
+
+namespace Depaumer
+{
+    public class AndroidWifiScanner : IWifiScanner
+    {
+        public AndroidWifiScanner()
+        {
+            LastWifiScanSignals = new AndroidWifiSignal[0];
+        }
+
+        public IWifiSignal[] LastWifiScanSignals { get; }
+
+        [Obsolete]
+        public bool TryPerformWifiScan(out IWifiSignal[] scannedSignals)
+        {
+            IWifiSignal[] signals = null; // Default value if scan failed
+            bool result = false;
+            try
+            {
+                result = MainActivity.wifiManager.StartScan();
+
+                if (result)
+                {
+                    IList<ScanResult> scans = MainActivity.wifiManager.ScanResults;
+                    signals = new IWifiSignal[scans.Count];
+                    for (int i = 0; i < scans.Count; i++)
+                    {
+                        ScanResult scan = scans[i];
+                        signals[i] = new AndroidWifiSignal(scan);
+                    }
+                }
+
+            }
+            finally
+            {
+                scannedSignals = signals;
+            }
+
+            return result;
+        }
+    }
+}
