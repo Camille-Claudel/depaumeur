e006e79b8d435cb59fe9f941c88f7b49ffac4019
Someone-42 <tristn.lemarchand@gmail.com>
2022-04-20 11:31:22 +0200

    MainActivity Internal variables for wifi and context

--- src/XamarinDepaumer/DepaumerAndroid/MainActivity.cs
+++ src/XamarinDepaumer/DepaumerAndroid/MainActivity.cs
@@ -7,18 +7,25 @@ using AndroidX.AppCompat.Widget;
 using AndroidX.AppCompat.App;
 using Google.Android.Material.FloatingActionButton;
 using Google.Android.Material.Snackbar;
+using Android.Net.Wifi;
+using Java;
+using Android.Content;
 
 namespace Depaumer
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        internal static WifiManager wifiManager;
+        internal static Context context;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
+            wifiManager = Application.ApplicationContext.GetSystemService(Context.WifiService) as WifiManager;
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
