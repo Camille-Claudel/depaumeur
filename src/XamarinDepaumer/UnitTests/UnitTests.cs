d66ab4b7c18b3f9b50ea664f998383f46a555a25
Remi Mazhar <remi.mazhar01@gmail.com>
2022-05-07 12:53:37 +0200

    add TestCalibrationSettings unit test

--- src/XamarinDepaumer/UnitTests/UnitTests.cs
+++ src/XamarinDepaumer/UnitTests/UnitTests.cs
@@ -16,5 +16,22 @@ namespace UnitTests
             Assert.AreEqual(pos, cp.Position);
             Assert.AreEqual(signals, cp.Signals);
         }
+
+        [TestMethod]
+        public void TestCalibrationSettings()
+        {
+            string[] macAddresses = { "a", "b", "c" };
+            CalibrationPoint[] calPts =
+            {
+                new CalibrationPoint(new Vector2(0, 0), new double[]{-50, -70, -70}),
+                new CalibrationPoint(new Vector2(0, 1), new double[]{-70, -50, -90}),
+                new CalibrationPoint(new Vector2(1, 1), new double[]{-90, -70, -70}),
+                new CalibrationPoint(new Vector2(1, 0), new double[]{-70, -90, -50})
+            };
+
+            CalibrationSettings cs = new CalibrationSettings(calPts, macAddresses);
+            Assert.AreEqual(cs.CalibrationPoints, calPts);
+            Assert.AreEqual(cs.WifiPointMacAddresses, macAddresses);
+        }
     }
 }
